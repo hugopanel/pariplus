3421ddb41e4ca57fd83dda3ee74071a7debf766c
Hugo PANEL <hugopanel@gmail.com>
2023-05-21 23:41:32 +0200

    Correction mauvais chemin

--- get_add.php
+++ get_add.php
@@ -1,9 +1,9 @@
 <?php
 
 function get_add() {
-    $path    = '/assets/img/ads';
+    $path    = 'assets/img/ads';
     $files = scandir($path);
     $files = array_diff(scandir($path), array('.', '..'));
 
-    return $files[array_rand($files)];
+    return "/assets/img/ads" . $files[array_rand($files)];
 }
